b521405cad434b5a757689c3db6167157564f7ec
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-08-02 00:58:49 +0900

    스레드 공부

---
+++ sec12/src/BeePrintExample1.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class BeePrintExample1 {
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for (int i = 0; i < 5; i++) {
+            toolkit.beep();
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+
+
+        }
+    }
+}
