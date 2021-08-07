611e9c95c28f38452a55e75038699e478995a075
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-08-07 21:55:02 +0900

    코드테스트 연습

---
+++ goorm_level_test/src/SimulationExample.java
@@ -0,0 +1,24 @@
+public class SimulationExample {
+    //수포자1,2,3배열생성
+    //1,2,3이 찍는 값 배열에 대입
+    //answer 의 값 입력
+    //answer의 값과 1,2,3의 배열안에 있는 값과 for문으로 비교
+    //점수 배열생성 answer 의 값과 1,2,3의 값이 같다면 점수배열에 값추가
+    //맞은 개수 출력
+
+    public String solution(int[] answer) {
+
+
+
+
+        }
+    }
+    public int[] compareAnswer(int[] answer){
+        int[] one = new int[]{1, 2, 3, 4, 5};
+        int[] two = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        int[] thr = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        for (int i=0;i<answer.length;i++){
+            
+    }
+}
