2c6543384ba9c4bc1b4f410c990d8b19aed19212
RohSeongHoon <scubarsh@naver.com>
2021-12-25 00:30:26 +0900

    스플링 프레임워크를 이용해서 바꾼코드 현제는 스프링 라이브러리가 없어 오류가뜸

--- spting_book/src/sec1/user/domain/DaoFactory.java
+++ spting_book/src/sec1/user/domain/DaoFactory.java
@@ -1,13 +1,12 @@
 package sec1.user.domain;
 
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
-//    public AccountDao accountDao(){
-//        return new AccountDao(connectionMaker());
-//    }
-
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new NConnectionMaker();
     }
--- spting_book/src/sec1/user/domain/UserDaoTest.java
+++ spting_book/src/sec1/user/domain/UserDaoTest.java
@@ -4,7 +4,8 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
-        System.out.println(dao);
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao",UserDao.class);
     }
 }
