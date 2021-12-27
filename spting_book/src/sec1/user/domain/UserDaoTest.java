48573fa99c98ba28e3610b66ef99e2167a682991
RohSeongHoon <scubarsh@naver.com>
2021-12-27 20:14:10 +0900

    일반 팩토리에서 생성한 코드와 스플링 컨텍스트에서 가지고온 오브젝트 출력 동일성과 동등성 차이 확인 동일성은 ==연산자이고 동등성은 equals 연산자이다

--- spting_book/src/sec1/user/domain/UserDaoTest.java
+++ spting_book/src/sec1/user/domain/UserDaoTest.java
@@ -9,13 +9,30 @@ public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao",UserDao.class);
-        User user = new User();
-        user.setId("spring");
-        user.setName("노성훈");
-        user.setPassword("123");
-        dao.add(user);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        //User user = new User();
+//        user.setId("spring");
+//        user.setName("노성훈");
+//        user.setPassword("123");
+//        dao.add(user);
+        DaoFactory factory = new DaoFactory();
 
-        System.out.println("등록성공 :"+dao.get(user.getId()));
+        UserDao dao1 = factory.userDao();
+        UserDao dao2 = factory.userDao();
+        System.out.println(dao1);
+        System.out.println(dao2);
+        //출력값
+        // sec1.user.domain.UserDao@553f17c
+        //sec1.user.domain.UserDao@4f7d0008
+        ApplicationContext context1 = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao3 = context1.getBean("userDao", UserDao.class);
+        UserDao dao4 = context1.getBean("userDao", UserDao.class);
+        System.out.println(dao3);
+        System.out.println(dao4);
+//        출력
+//        sec1.user.domain.UserDao@7085bdee
+//        sec1.user.domain.UserDao@7085bdee
+
+
     }
 }
