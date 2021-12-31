04be108024aadd0fed22c0d58472bfb5c61bd459
Noh Seong Hoon <scubarsh@naver.com>
2022-01-01 02:09:03 +0900

    스프링의 IOC 보다 조금더 핵심인 DI , 런타임 의존관계 주입하기 전의 상태를 보여준다

--- spting_book/src/sec1/user/domain/UserDao.java
+++ spting_book/src/sec1/user/domain/UserDao.java
@@ -5,9 +5,15 @@ import java.sql.*;
 public class UserDao {
     private ConnectionMaker connectionMaker;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao() {
+        connectionMaker = new NConnectionMaker(); // 관계설정 책임분리 전의 생성자
+        //UserDao는 설계시점에서 NConnectionMaker이라는 구체적인 클래스의 존재를 알고있음
+        //Connection인터페이스의 관계와 런타임 의존관계까지 결정 관리하는 상태임  매우 좋지않은 코드 
     }
+
+    //    public UserDao(ConnectionMaker connectionMaker) {
+//        this.connectionMaker = connectionMaker;
+//    }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection c = connectionMaker.makeConnection(); //인터페이스에 정의된걸 사용해서 클래스가 바뀌어도 메소도이름이 변경될 걱정은없다
