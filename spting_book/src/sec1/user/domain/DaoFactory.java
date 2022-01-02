2056968d0ea8d9b081721c2ec5078e2f8d6ba9a3
Noh Seong Hoon <scubarsh@naver.com>
2022-01-02 17:22:23 +0900

    DaoFactory에서 DB에 연결한 횟수를 세주는 CountingConnectionMaker를 사용할수있도록 Bean생성

--- spting_book/src/sec1/user/domain/DaoFactory.java
+++ spting_book/src/sec1/user/domain/DaoFactory.java
@@ -10,7 +10,11 @@ public class DaoFactory {
     }
     @Bean
     public ConnectionMaker connectionMaker(){
-        return new NConnectionMaker();
+    return new CountingConnectionMaker(realConnectionMaker());
     } //DI 방식을 적용하면 여기부분을 변경하는것만으로도 db를 변경할수있다
     //효율이 높아짐
+    @Bean
+    public ConnectionMaker realConnectionMaker(){
+        return new NConnectionMaker();
+    }
 }
