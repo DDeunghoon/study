27c50a0e251beb405bb04a64b83ea63b0e4e5c94
Noh Seong Hoon <scubarsh@naver.com>
2022-01-02 17:56:41 +0900

    UserDaoCountingConnection 테스트 클래스 생성및 생성자가 아닌 setter메소드를 이용해서 DI를 사용해서 UserDao타입의 빈을 만드는 DaoFactory의 UserDao()메소드 생성

---
+++ spting_book/src/sec1/user/domain/CountingDaoFactory.java
@@ -0,0 +1,28 @@
+package sec1.user.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CountingDaoFactory {
+
+
+    @Bean
+    public UserDao userDao() {
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
+        return userDao();
+    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new CountingConnectionMaker(realConnectionMaker());
+    } //DI 방식을 적용하면 여기부분을 변경하는것만으로도 db를 변경할수있다
+
+    //효율이 높아짐
+    @Bean
+    public ConnectionMaker realConnectionMaker() {
+        return new NConnectionMaker();
+    }
+}
+
--- spting_book/src/sec1/user/domain/DaoFactory.java
+++ spting_book/src/sec1/user/domain/DaoFactory.java
@@ -6,15 +6,12 @@ import org.springframework.context.annotation.Configuration;
 public class DaoFactory {
     @Bean
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
+        return userDao;
     }
     @Bean
     public ConnectionMaker connectionMaker(){
-    return new CountingConnectionMaker(realConnectionMaker());
-    } //DI 방식을 적용하면 여기부분을 변경하는것만으로도 db를 변경할수있다
-    //효율이 높아짐
-    @Bean
-    public ConnectionMaker realConnectionMaker(){
         return new NConnectionMaker();
     }
 }
