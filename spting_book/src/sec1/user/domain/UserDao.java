3b6bc9eacfdf0eaf8f43ec97aa0a11f1c6dd96b7
Noh Seong Hoon <scubarsh@naver.com>
2022-01-03 20:11:46 +0900

    ConnectionMaker을 사용하는 대신 자바에서 이미 만들어놓은 DataSource 인터페이스를 사용할 연습

--- spting_book/src/sec1/user/domain/UserDao.java
+++ spting_book/src/sec1/user/domain/UserDao.java
@@ -2,18 +2,19 @@ package sec1.user.domain;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
 
-    public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public void setConnectionMaker(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection(); //인터페이스에 정의된걸 사용해서 클래스가 바뀌어도 메소도이름이 변경될 걱정은없다
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id,name,password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -27,7 +28,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
         ps.setString(1, id);
