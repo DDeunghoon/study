427757cf525a2a9bcf17cd467e321b11d2072c2e
Noh Seong Hoon <scubarsh@naver.com>
2022-01-05 18:19:09 +0900

    dataSource를 xml형식으로 변경방법  주석으로 정리

--- spting_book/src/sec1/user/domain/DaoFactory.java
+++ spting_book/src/sec1/user/domain/DaoFactory.java
@@ -15,14 +15,20 @@ public class DaoFactory {
         return userDao;
     }
 
-    @Bean
-    public DataSource dataSource() {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    @Bean                               //xml로 변경
+    public DataSource dataSource() { //<bean id="dataSource" class ="sec1.user.domain.SimpleDriverDataSource">
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource(); //SimpleDriverDataSource 를 생성은 할 수 있지만 .set~부분은 추가할 수가 없다 이때
 
-        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/spring");
-        dataSource.setUsername("root");
-        dataSource.setPassword("qsc20215");
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class); //<bean id="dataSource" class ="sec1.user.domain.SimpleDriverDataSource">
+        dataSource.setUrl("jdbc:mysql://localhost/spring"); //<property name="diverClass" value="com.mysql.jdbc.Driver"/> class부분 생략 타입변환을 해줌
+        dataSource.setUsername("root"); //<property name="url" value="jdbc:mysql://localhost/spring"/>
+        dataSource.setPassword("qsc20215"); //property name="password" value="qsc20215"/>
         return dataSource;
     }
+
+
+
+
+
+
 }
