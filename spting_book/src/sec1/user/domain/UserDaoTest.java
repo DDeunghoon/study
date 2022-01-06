83050c81f8b2828559d9240d40fe79717c1b2ede
Noh Seong Hoon <scubarsh@naver.com>
2022-01-06 17:41:00 +0900

    jUnit 프레임워크 추가

--- spting_book/src/sec1/user/domain/UserDaoTest.java
+++ spting_book/src/sec1/user/domain/UserDaoTest.java
@@ -2,11 +2,12 @@ package sec1.user.domain;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-
+import org.junit.Test;
 import java.sql.SQLException;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
         User user = new User();
@@ -17,14 +18,14 @@ public class UserDaoTest {
 
         User user2 = dao.get(user.getId());
         System.out.println("등록 성공!! = "+user2.getId());
-        if (!user.getName().equals(user2.getName())){
+        assertThat()
             System.out.println("테스트 실패 (name)");
-        }
+
         if (!user.getPassword().equals(user2.getPassword())) {
             System.out.println("테스트 실패 (password)");
         }
         else {
         System.out.println("조회 성공");}
-
     }
+
 }
