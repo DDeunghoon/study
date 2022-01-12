d82a5b33baf2030be9df4dec95b45d7bb2879ca7
Noh Seong Hoon <scubarsh@naver.com>
2022-01-12 23:13:44 +0900

    JUnit이 테스트 컨텍스를 테스트를 진행할때 한개만 만드는지 검증하기위한 xml파일 생성 context를 몇개만드는지 확인하기 위해서 생성한것이기 때문에 bean을 추가할 필요는없다

--- spting_book/src/sec2/learningtest/junit/JUnitTest.java
+++ spting_book/src/sec2/learningtest/junit/JUnitTest.java
@@ -1,27 +1,32 @@
 package sec2.learningtest.junit;
 
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObject = new HashSet<JUnitTest>();
 
     @Test
     public void test1(){
-        assertThat(this,is(not(sameInstance(testObject))));
-        testObject=this;
+        assertThat(testObject,is(not(hasItem(this))));
+        testObject.add(this);
     }
     @Test
     public void test2(){
-        assertThat(this,is(not(sameInstance(testObject))));
-        testObject=this;
+        assertThat(testObject,is(not(hasItem(this))));
+        testObject.add(this);
     }
     @Test
     public void test3(){
-        assertThat(this,is(not(sameInstance(testObject))));
-        testObject=this;
+        assertThat(testObject,is(not(hasItem(this))));
+        testObject.add(this);
     }
 }
