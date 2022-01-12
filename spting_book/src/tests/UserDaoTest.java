5af78af371c5ce3f38b9a89fb365dec5866aa3f1
Noh Seong Hoon <scubarsh@naver.com>
2022-01-12 16:56:24 +0900

    xml 파일 위치 이동및 dataSource 의 class의 경로 설정 오류 해결

--- spting_book/src/tests/UserDaoTest.java
+++ spting_book/src/tests/UserDaoTest.java
@@ -1,31 +1,27 @@
 import org.junit.Before;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import sec1.user.domain.User;
 import sec1.user.domain.UserDao;
 import java.sql.SQLException;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/applicationContext.xml")
 
 public class UserDaoTest {
     private UserDao dao;
     private User user1;
     private User user2;
     private User user3;
-    private ApplicationContext context;
+
+
     @Before
     public void setUp() {
-        this.dao = this.context.getBean("userDao", UserDao.class);
+        ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
+        this.dao = context.getBean("userDao", UserDao.class);
         this.user1 = new User("아이디2","이름1","비번1");
         this.user2 = new User("아이디3","이름2","비번2");
         this.user3 = new User("아이디4","이름3","비번3");
