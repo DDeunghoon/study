4ffd0cde0eafde65ffb656a0e8aef3cd5755d21d
Noh Seong Hoon <scubarsh@naver.com>
2022-01-14 21:47:01 +0900

    AddStatement클래스는 UserDao 내부의 메소드와 결합이 강하고 사실 UserDao에서 밖에 사용하지않는다  그러므로 AddStatement클래스를 로컬 클래스로 만들어 생성자를 제거하고 파라메터를 직접사용한다면 클래스도 더이상 늘지않고 보기도 좋은 코드가 되었다

--- spting_book/src/sec1/user/domain/UserDao.java
+++ spting_book/src/sec1/user/domain/UserDao.java
@@ -18,9 +18,20 @@ public class UserDao {
     }
 
 
-    public void add(User user) throws SQLException {
-       StatementStrategy st = new AddStatement(user);
-       jdbcContextWithStatementStrategy(st);
+    public void add(final User user) throws SQLException {
+        class AddStatement implements StatementStrategy {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("INSERT INTO users(id,name,password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        }
+        StatementStrategy st = new AddStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
