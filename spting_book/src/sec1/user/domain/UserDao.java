64b510e9091c73d2d1e0884bd62b80480ed6f0af
Noh Seong Hoon <scubarsh@naver.com>
2022-01-14 21:58:26 +0900

    Userdao의 add메소드에서만 클래스가 필요한것이라면 로컬 클래스도 괜찮지만 익명함수를 사용하는 편이 더 간결하다 add메소드에 적용

--- spting_book/src/sec1/user/domain/UserDao.java
+++ spting_book/src/sec1/user/domain/UserDao.java
@@ -19,19 +19,18 @@ public class UserDao {
 
 
     public void add(final User user) throws SQLException {
-        class AddStatement implements StatementStrategy {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("INSERT INTO users(id,name,password) values(?,?,?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
+        jdbcContextWithStatementStrategy(
+                 new StatementStrategy() {
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement("INSERT INTO users(id,name,password) values(?,?,?)");
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
 
-                return ps;
-            }
-        }
-        StatementStrategy st = new AddStatement();
-        jdbcContextWithStatementStrategy(st);
+                        return ps;
+                    }
+                }
+        );
     }
 
     public User get(String id) throws SQLException {
