e7d290875928a2573e7cf4cd924f6d5222bacb6e
Noh Seong Hoon <scubarsh@naver.com>
2022-01-19 16:47:19 +0900

    getCount() 메소드에도 적용

--- spting_book/src/user/dao/UserDao.java
+++ spting_book/src/user/dao/UserDao.java
@@ -6,9 +6,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import user.domain.StatementStrategy;
 import user.domain.User;
 
@@ -30,7 +32,7 @@ public class UserDao {
 
     public void add(final User user) throws SQLException {
         this.jdbcTemplate.update("insert into users(id,name,password) value(?,?,?)",
-                user.getId(),user.getName(),user.getPassword());
+                user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws SQLException {
@@ -62,40 +64,17 @@ public class UserDao {
 
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("SELECT COUNT(*) FROM users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-
-                }
+        return this.jdbcTemplate.query(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                return connection.prepareStatement("select count(*) from users");
             }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
+        }, new ResultSetExtractor<Integer>() {
+            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+                rs.next();
+                return rs.getInt(1);
             }
-            if (c != null) {
-                try {
-
-                    c.close();
-                } catch (SQLException e) {
-                    throw e;
-                }
-            }
-        }
+        });
     }
 }
 
