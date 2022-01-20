b7674122c8351ebdf3d8e1b534db0de2cd0005a3
Noh Seong Hoon <scubarsh@naver.com>
2022-01-20 16:28:48 +0900

    getAll()메소드에 대한 테스트 코드 생성

--- spting_book/src/tests/UserDaoTest.java
+++ spting_book/src/tests/UserDaoTest.java
@@ -11,6 +11,7 @@ import user.domain.User;
 import user.dao.UserDao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -29,8 +30,8 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        System.out.println("=================="+ this.context);
-        System.out.println("==================="+this);
+        System.out.println("==================" + this.context);
+        System.out.println("===================" + this);
         this.dao = context.getBean("userDao", UserDao.class);
         this.user1 = new User("user1", "asdf1", "asdf1");
         this.user2 = new User("user2", "asdfasdf2", "asdfw2");
@@ -42,17 +43,41 @@ public class UserDaoTest {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-
         dao.add(user1);
         dao.add(user2);
         dao.add(user3);
+
         assertThat(dao.getCount(), is(3));
 
+        System.out.println(dao.get(user1.getId()));
+    }
 
+    @Test
+    public void getAll() throws SQLException {
+        dao.deleteAll();
 
-        System.out.println(dao.get(user1.getId())); 
+        dao.add(user1);
+        List<User> user1 = dao.getAll();
+        assertThat(user1.size(), is(1));
+        checkSameUser(user1, user1.get(0));
 
+        dao.add(user2);
+        List<User> user2 = dao.getAll();
+        assertThat(user2.size(), is(2));
+        checkSameUser(user1, user1.get(0));
+        checkSameUser(user2, user2.get(1));
 
+        dao.add(user3);
+        List<User> user3 = dao.getAll();
+        assertThat(user3.size(), is(3));
+        checkSameUser(user1, user1.get(0));
+        checkSameUser(user2, user1.get(1));
+        checkSameUser(user3, user1.get(2));
+    }
+    private void checkSameUser(User user1,User user2){
+        assertThat(user1.getId(),is(user2.getId()));
+        assertThat(user2.getName(),is(user2.getName()));
+        assertThat(user1.getPassword(),is(user2.getPassword()));
     }
 
     @Test
