8dad14b1c2221161a1d2d8d19cf617bbd8904ac0
Noh Seong Hoon <scubarsh@naver.com>
2022-01-23 16:09:19 +0900

    사용자아이디가 중복됐을 때 사용하는 DuplicateUserIdException 생성 RuntimeException을 상속해서 런타임 예외로 만든다 필요하다면 언제든지 잡아서 처리할 수있게 만들기 위함 중첩예외를 위해 생성자를 추가하는 것도 잊으면 안됨

---
+++ spting_book/src/exceptionTest/DuplicateUserIdException.java
@@ -0,0 +1,7 @@
+package exceptionTest;
+
+public class DuplicateUserIdException extends RuntimeException{
+    public DuplicateUserIdException(Throwable cause){
+        super(cause);
+    }
+}
--- spting_book/src/exceptionTest/Translation.java
+++ spting_book/src/exceptionTest/Translation.java
@@ -1,22 +1,22 @@
-package exceptionTest;
-
-import com.mysql.cj.exceptions.MysqlErrorNumbers;
-
-import java.sql.SQLException;
-
-public class Translation {
-    public void add(User user) throws DuplecateUserIdException, SQLException {
-        try {
-            //JDBC를 이용해 user 정보를 DB에 추가하는 코드
-            //또는 그런 기능을 가진 다른 SQLException을 던지는 메소드를 호춣하는 코드
-        } catch (SQLException) {
-            //에러코드가 sql의 "Duplicate Entry(1062) 라면 예외 전환
-            if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
-                throw DuplicateUserIdException();
-            } else {
-                //그 외의 경우에는 SQLException을 그대로 던진다 
-                throw e;
-            }
-        }
-    }
-}
+//package exceptionTest;
+//
+//import com.mysql.cj.exceptions.MysqlErrorNumbers;
+//
+//import java.sql.SQLException;
+//
+//public class Translation {
+//    public void add(User user) throws DuplecateUserIdException, SQLException {
+//        try {
+//            //JDBC를 이용해 user 정보를 DB에 추가하는 코드
+//            //또는 그런 기능을 가진 다른 SQLException을 던지는 메소드를 호춣하는 코드
+//        } catch (SQLException) {
+//            //에러코드가 sql의 "Duplicate Entry(1062) 라면 예외 전환
+//            if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+//                throw DuplicateUserIdException();
+//            } else {
+//                //그 외의 경우에는 SQLException을 그대로 던진다
+//                throw e;
+//            }
+//        }
+//    }
+//}
